5af5bb6ed2e1a60a099ba449a25b3490e459b209
idha444 <idhazhiya@gmail.com>
2026-02-26 08:57:55 +0530

    UC9:Hardcoded palindrome

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC9.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        String input = sc.nextLine();
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Is Palindrome? : " + result);
+
+        sc.close();
+    }
+
+    /**
+     * Recursively checks whether a string is palindrome.
+     *
+     * @param s     Input string
+     * @param start Starting index
+     * @param end   Ending index
+     * @return true if palindrome, otherwise false
+     */
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If mismatch found
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return check(s, start + 1, end - 1);
+    }
+}
